06782cd02616d7fb16fa6cf09c0f1d1976a8fe3c
coblee <chocobo@alum.mit.edu>
2011-10-16 12:50:24 -0700

    Update Mac makefiles.

--- contrib/create_osx_dmg.sh
+++ contrib/create_osx_dmg.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-# 
-# Creates a Bitcoin.dmg OSX file from the contrib/BitcoinTemplate.dmg file
+# Litecoin
+# Creates a Litecoin.dmg OSX file from the contrib/LitecoinTemplate.dmg file
 #
 # Recipe from: http://digital-sushi.org/entry/how-to-create-a-disk-image-installer-for-apple-mac-os-x/
 #
-# To make a prettier BitcoinTemplate.dmg:
-#  + open (mount) BitcoinTemplate.dmg
+# To make a prettier LitecoinTemplate.dmg:
+#  + open (mount) LitecoinTemplate.dmg
 #  + change the file properties, icon positions, background image, etc
-#  + eject, then commit the changed BitcoinTemplate.dmg
+#  + eject, then commit the changed LitecoinTemplate.dmg
 #
 
 CWD=$(pwd)
@@ -17,31 +17,32 @@ if [ $# -lt 1 ]; then
     then
         TOP=$(dirname $CWD)
     else
-        echo "Usage: $0 /path/to/bitcoin/tree"
+        echo "Usage: $0 /path/to/litecoin/tree"
         exit 1
     fi
 else
     TOP=$1
 fi
 
-# Create Bitcoin-Qt.app
+# Create Litecoin-Qt.app
 cd "$TOP"
-if [ ! -e Makefile ]; then qmake bitcoin-qt.pro; fi
+if [ ! -e Makefile ]; then qmake litecoin-qt.pro; fi
 make
-macdeployqt Bitcoin-Qt.app
+macdeployqt Litecoin-Qt.app
 # Workaround a bug in macdeployqt: https://bugreports.qt.nokia.com/browse/QTBUG-21913
 # (when fixed, this won't be necessary)
-cp /opt/local/lib/db48/libdb_cxx-4.8.dylib Bitcoin-Qt.app/Contents/Frameworks/
+cp /opt/local/lib/db48/libdb_cxx-4.8.dylib Litecoin-Qt.app/Contents/Frameworks/
 install_name_tool -id @executable_path/../Frameworks/libdb_cxx-4.8.dylib \
-    Bitcoin-Qt.app/Contents/Frameworks/libdb_cxx-4.8.dylib
+    Litecoin-Qt.app/Contents/Frameworks/libdb_cxx-4.8.dylib
 install_name_tool -change libqt.3.dylib \
         @executable_path/../Frameworks/libqt.3.dylib \
-        Bitcoin-Qt.app/Contents/MacOS/Bitcoin-Qt
+        Litecoin-Qt.app/Contents/MacOS/Litecoin-Qt
 
 # Create a .dmg
-macdeployqt Bitcoin-Qt.app -dmg
+macdeployqt Litecoin-Qt.app -dmg
 
-# Compile bitcoind
+# Compile litecoind
 cd "$TOP/src"
+rm litecoind
 STATIC=1 make -f makefile.osx
 
