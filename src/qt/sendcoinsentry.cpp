202b5bd9073a938eb76cd2a84e56ad87c46756a6
Michael Ford <fanquake@gmail.com>
2012-03-08 14:27:04 +0800

    Only fill in label from address book, if no label is filled in yet

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -59,7 +59,10 @@ void SendCoinsEntry::on_payTo_textChanged(const QString &address)
 {
     if(!model)
         return;
-    ui->addAsLabel->setText(model->getAddressTableModel()->labelForAddress(address));
+
+    // Fill in label from address book, if no label is filled in yet 
+    if(ui->addAsLabel->text().isEmpty()) 
+        ui->addAsLabel->setText(model->getAddressTableModel()->labelForAddress(address));
 }
 
 void SendCoinsEntry::setModel(WalletModel *model)
