111085aa64fcc83ee4a1e2eb06557d39d6d880c0
fanquake <fanquake@gmail.com>
2012-03-08 18:39:24 +0800

    Change a few Bitcoin references

--- src/qt/sendcoinsentry.h
+++ src/qt/sendcoinsentry.h
@@ -9,7 +9,7 @@ namespace Ui {
 class WalletModel;
 class SendCoinsRecipient;
 
-/** A single entry in the dialog for sending bitcoins. */
+/** A single entry in the dialog for sending litecoins. */
 class SendCoinsEntry : public QFrame
 {
     Q_OBJECT
